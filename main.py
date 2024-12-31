4f9dbe16976b0dc6253af06f59b851da006d5f26
Aditya Acharya <aditiya.acharya910@gmail.com>
2024-12-31 15:35:46 -0500

    added the key listners to move snake

--- main.py
+++ main.py
@@ -6,19 +6,22 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
-
 screen.tracer(0)
 
-
 snake = Snake()
-game_is_on = True
+screen.listen()
 
+screen.onkey(snake.up,"Up")
+screen.onkey(fun=snake.down,key='Down')
+screen.onkey(fun=snake.right,key='Right')
+screen.onkey(fun=snake.left,key='Left')
+
+
+game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
-
 
 screen.listen()
 screen.exitonclick()
